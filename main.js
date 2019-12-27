a94ab86865f37df2f4deeddb9e59aff90e758885
Nick Woods <nick.woods.wi@gmail.com>
2019-12-27 00:38:52 -0600

    added listing previews, navbar, footer

---
+++ main.js
@@ -0,0 +1,103 @@
+//Pull source code for a given URL
+//Copy addresses for all search results
+//Get ZPID for all addresses from GetDeepSearchResults
+//Get street address, city, state, zip code, price, photo URLS, bedrooms, bathrooms, square footage, home description
+//Build HTML
+
+const searchUrl = 'https://www.firstweber.com/homes-for-sale/Shorewood_combo/P_lp-cd-presentation/sc_l_listing_price+DESC/sd_S2/page_1//nts_100/';
+
+function loadPropertyDetails() {
+    $.get(searchUrl, function(data) {
+
+        let streetAddresses = [];
+        let cityStates = [];
+        let prices = [];
+        let editedDetails = [];
+
+        $.each($(data).find('div.address'), function(i, val) {
+            streetAddresses.push(val.innerText);
+        });
+        $.each($(data).find('div.city'), function(i, val) {
+            cityStates.push(val.innerText);
+        });
+        $.each($(data).find('div.price'), function(i, val) {
+            prices.push((val.innerText).trim());
+        });
+        $.each($(data).find('div.bedbath'), function(i, val) {
+            let propertyDetails = val.innerText.trim();
+            let indexedDetails = propertyDetails.split("");
+            let detailObject = {br: indexedDetails[0], fullbath: indexedDetails[27],            halfbath: indexedDetails[48]};
+            editedDetails.push(detailObject);
+        });
+        compileData(streetAddresses, cityStates, prices, editedDetails);
+    });
+};
+
+function compileData(streetAddresses, cityStates, prices, editedDetails) {
+    
+    let propertiesArray = [];
+
+    for (let i = 0; i < streetAddresses.length; i++) {
+
+        let property = {
+             'city': cityStates[i],
+             'rooms': editedDetails[i],
+             'prices': prices[i],
+             'street': streetAddresses[i],
+        };
+
+        propertiesArray.push(property);
+    };
+    filterProperties(propertiesArray);
+};
+
+function filterProperties(propertiesArray) {
+    let filteredArray = [];
+    for (i = 0; i < propertiesArray.length; i++) {
+        if (propertiesArray[i].city && 
+            propertiesArray[i].rooms && 
+            propertiesArray[i].prices && 
+            propertiesArray[i].street) 
+                {filteredArray.push(propertiesArray[i])}
+        };
+    buildPreviews(filteredArray);
+};
+
+function buildPreviews(filteredArray) {
+
+    for (i = 0; i < filteredArray.length; i++) {
+
+    let $city = filteredArray[i].city; 
+    let $price = filteredArray[i].prices;
+    let $street = filteredArray[i].street;
+
+    let $br = filteredArray[i].rooms.br;
+    let $bath = filteredArray[i].rooms.fullbath;
+    let $halfbath = filteredArray[i].rooms.halfbath;
+
+        let payload = 
+            
+    `<div class="listing">
+        <div class="listing-img-container"><img class="listing-img" src="listing-photos/3131-w-donges-bay-rd.jpg"></div>
+        <div class="listing-neighborhood">${$city}</div>
+        <div class="listing-specs">${$br} bed / ${$bath} bath / ${$halfbath} half-bath</div>
+        <div class="listing-price">${$price}<div>
+        <div class="listing-address">${$street}</div>
+        <div class="listing-links">SCHEDULE A SHOWING | VIEW FULL LISTING</div>
+    </div>`;
+
+        $('.listings').append(payload);
+
+    };
+
+};
+
+function callZillow() {
+//streetAddresses and cityState are used to call Zillow API
+}
+
+function buildPages() {
+//Zillow API result is used to build new pages, place on SRG site, and update sitemap
+};
+
+loadPropertyDetails();
